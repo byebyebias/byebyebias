0a9c250f35c79ca00f37d433983d52d6adc85fa7
aakshataa <121204544+aakshataa@users.noreply.github.com>
2024-11-01 00:12:25 -0400

    added tests for components

--- frontend/src/components/HeroSection/HeroSection.tsx
+++ frontend/src/components/HeroSection/HeroSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../Button/Button.tsx'; // Import the button component
+import Button from '../Button/Button'; // Import the button component
 import './HeroSection.css'
 
 type HeroSectionProps = {
@@ -14,6 +14,8 @@ const HeroSection:React.FC<HeroSectionProps> = ({ heading, body, buttonLabel, on
         <div style={{ display: 'flex', alignItems: 'left', justifyContent: 'left'}}>
             <h1 className="hero-heading">{heading}</h1>
             <p className="hero-body">{body}</p>
+
+            {/* Create button component using HeroSection Props */}
             <Button label={buttonLabel} onClick={onClick} /> 
         </div>
         

76c1351fa491bac67b22d4e7660f6ec3031b1643
Michelle <michh2005@gmail.com>
2024-11-02 06:22:32 -0400

    create temporary template of values for overview component

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -2,15 +2,24 @@ import {
     Typography,
     Stack,
  } from "@mui/material"
-
+import { useEffect } from "react"
 import Overview from "../../components/dashboard-components/Overview/Overview"
 
+const data = {
+    bias_score: "C+",
+    top_percentile: 50,
+    top_category: "ABC",
+    improvement_area: "BCD",
+}
 
-function DashboardPage() {
+function DashboardPage() { 
+
     return (
-        <Stack>
-            <Typography variant="h1">Dashboard</Typography>
+        <Stack sx={{p:20}}>
+            <Typography variant="h3">Dashboard</Typography>
             <Overview/>
+            
+
         </Stack>
     )
 }

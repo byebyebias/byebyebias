03bdf2900b8aecf876736afdf9ef6543ed3afafd
Michelle <michh2005@gmail.com>
2024-11-03 15:49:33 -0500

    moved bias_metrics file to api folder for now since can't figure out how to import from upper directories

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from "../actions/bias_metrics" import get_bias_metrics
 
 @api_view(['GET'])
 def hello_world(request):
     return Response({"message": "ByeByeBias :)"})
+
+@api_view(["GET"])
+def get_dashboard_data():
+    bias_metrics = 

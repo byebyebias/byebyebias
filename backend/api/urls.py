17b786500165fa7816b313d15d04629a8bce415f
Michelle <michh2005@gmail.com>
2024-11-03 17:50:11 -0500

    created route for metric, moved parquet to backend for testing

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, get_dashboard_data
 
 urlpatterns = [
     path('hello/', hello_world, name='hello_world'),
+     path('metrics/', get_dashboard_data, name='get_dashboard_data'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,41 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from "../actions/bias_metrics" import get_bias_metrics
+from backend.actions.bias_metrics import get_bias_metrics
+from backend.actions.converter import Converter
 
 @api_view(['GET'])
 def hello_world(request):
     return Response({"message": "ByeByeBias :)"})
 
 @api_view(["GET"])
-def get_dashboard_data():
-    bias_metrics = 
+def get_dashboard_data(request):
+    TEMP_TEST_FILE = "/Users/michelle/UofT_Stuff/Year2/CSC207/tripleb/backend/api/transaction_triple_b.parquet"
+    PROTECTED_ATTRIBUTE = 'sender_race'
+
+    fileConverter = Converter(TEMP_TEST_FILE)
+    print(        type(fileConverter.get_true_df()))
+    print(        type(fileConverter.get_pred_df()))
+
+    # Format is {metric: float}
+    bias_metrics = get_bias_metrics(
+        fileConverter.get_true_df(), 
+        fileConverter.get_pred_df(), 
+        PROTECTED_ATTRIBUTE
+    )
+    print(bias_metrics)
+
+    return Response({
+            "file_name": TEMP_TEST_FILE,
+            "overview": {
+                "score": "A+",
+                "bias_score": "C+",
+                "top_percentile": 50,
+                "top_category": "ABC",
+                "improvement_areas": ["BCD", "CDE"],
+
+            },
+            "metric_results": bias_metrics,
+
+            }
+        )

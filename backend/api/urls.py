9a84b76c3781b50c5a2532e79c1ce1658e420c49
Dhairya Thakkar <dhairyathakkar25@gmail.com>
2024-11-03 16:38:34 -0500

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,6 @@ from .views import statistical_parity
 
 urlpatterns = [
     path('hello/', hello_world, name='hello_world'),
-    path('spd/', statistical_parity, name='spd')
+    path('spd/', statistical_parity, name='spd'), 
+     path('upload/', upload_file, name="upload_file")
 ]

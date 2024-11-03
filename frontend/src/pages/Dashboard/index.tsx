6faedc4603f8d5ac9a089c12fb6d341b49a8cd66
Michelle <michh2005@gmail.com>
2024-11-03 15:40:00 -0500

    put dashboard parts into its own coloured section

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import { 
     Typography,
     Stack,
-    Container
+    Box
  } from "@mui/material"
 import Overview from "../../components/dashboard-components/Overview/Overview"
 import GraphGrid from "../../components/dashboard-components/GraphGrid/GraphGrid"
@@ -11,17 +11,17 @@ import { dataScore, dataOverview, dataGraphs, dataset_name } from "./data"
 function DashboardPage() { 
 
     return (
-        <Stack p={20}>
+        <Stack>
             <Typography variant="body2" pb={3}>{dataset_name}</Typography>
-            <Typography variant="h3" pb={3}>Dashboard</Typography>
 
-            <Stack direction="row" spacing={5}>
-                <BiasScore score={dataScore}/>
-                <Overview data={dataOverview}/>
-            </Stack>
+            <Box bgcolor="#E6EEF5" p={10}>
+                <Stack direction="row" spacing={5}>
+                    <BiasScore score={dataScore}/>
+                    <Overview data={dataOverview}/>
+                </Stack>
+                <GraphGrid graphsInfo={dataGraphs}/>
 
-            <GraphGrid graphsInfo={dataGraphs}/>
-
+            </Box>
 
         </Stack>
     )

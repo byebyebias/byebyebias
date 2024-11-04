46d1e55dd31df18129560e9e25d84c43cf643691
Michelle <michh2005@gmail.com>
2024-11-04 01:52:15 -0500

    made dashboard more similar to figma

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,23 +1,29 @@
 import { 
     Typography,
     Stack,
+    Box
  } from "@mui/material"
 import { useLocation } from 'react-router-dom';
 import Overview from "../../components/dashboard-components/Overview/Overview"
 import GraphGrid from "../../components/dashboard-components/GraphGrid/GraphGrid"
-// import { dataOverview, dataGraphs, dataset_name } from "./data"
+import BiasScore from "../../components/dashboard-components/BiasScore/BiasScore";
 
 function DashboardPage() { 
     const location = useLocation();
     const { dashboardData } = location.state
 
     return (
-        <Stack p={20}>
+        <Stack>
             <Typography variant="body2" pb={3}>{dashboardData.file_name}</Typography>
-            <Typography variant="h3" pb={3}>Dashboard</Typography>
-            <Overview data={dashboardData.overview}/>
-            <GraphGrid graphsInfo={dashboardData.metric_results}/>
 
+            <Box bgcolor="#E6EEF5" p={10}>
+                <Stack direction="row" spacing={5}>
+                    <BiasScore score={dashboardData.overview.score}/>
+                    <Overview data={dashboardData.overview}/>
+
+                </Stack>
+                    <GraphGrid graphsInfo={dashboardData.metric_results}/>
+            </Box>
         </Stack>
     )
 }

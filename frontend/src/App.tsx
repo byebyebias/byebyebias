2e2e4b39fbb89907b3d17b7cc46bdf4b53b93578
chloehylee <335732582@gapps.yrdsb.ca>
2024-11-12 18:54:53 -0500

    fix role issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import Button from "./components/Button/Button";
 import Footer from "./components/Footer/Footer";
 import HeroSection from "./components/HeroSection/HeroSection";
 import ProcessStep from "./components/ProcessStep/ProcessStep";
-import Table from "./components/Table/Table"
+import Table from "./components/Table/Table";
 import FileUpload from "./components/FileUpload/FileUpload";
 
 const apiUrl = import.meta.env.VITE_API_URL;
@@ -41,34 +41,38 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      {/* Put heading section here and mark with "header" when that PR is merged */}
-      {/* <header role="title">…</header> */}
+      {/* Use header for top-level heading */}
+      <header>
+        <HeroSection
+          heading="Target fraud detection bias at the source."
+          body="Some sort of subtitle text here. Check out our handy guide ↓ if you need any help!"
+        />
+      </header>
 
-      <main role="herosection">
-      <HeroSection
-        heading="Target fraud detection bias at the source."
-        body="Some sort of subtitle text here. Check out our handy guide ↓ if you need any help!"
-      />
-      </main> 
-      {/* Process Step / Instruction Section */}
-      <ProcessStep
-        title="Upload your dataset."
-        stepnum={1}
-        body="The format of the dataset must be a parquet file with the following mandatory columns highlighted in the table below."
-      />
-      <Table />
-      <aside role = "userguide">
-      <ProcessStep
-        title="View our feedback."
-        stepnum={2}
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean molestie mi id sapien posuer."
-      />
-      <ProcessStep
-        title="Adjust and try again!"
-        stepnum={3}
-        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean molestie mi id sapien posuer."
-      />
-       </aside>
+      <main>
+        {/* Main content, including the process steps */}
+        <ProcessStep
+          title="Upload your dataset."
+          stepnum={1}
+          body="The format of the dataset must be a parquet file with the following mandatory columns highlighted in the table below."
+        />
+        <Table />
+      </main>
+
+      <aside aria-labelledby="user-guide-heading">
+        {/* Additional information or side content */}
+        <h2 id="user-guide-heading">User Guide</h2>
+        <ProcessStep
+          title="View our feedback."
+          stepnum={2}
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean molestie mi id sapien posuer."
+        />
+        <ProcessStep
+          title="Adjust and try again!"
+          stepnum={3}
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean molestie mi id sapien posuer."
+        />
+      </aside>
 
       <Footer label="© 2024 Team TripleB" />
     </div>

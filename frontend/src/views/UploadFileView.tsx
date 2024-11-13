6c98d3f3778305a00e04d65549ea35427648fef9
Helen <129004013+1zhaohel@users.noreply.github.com>
2024-11-12 19:16:54 -0500

    Refactor to Clean Architecture (#38)
    * deleted unused files
    
    * clean up unused files
    
    * clean up
    
    * frontend refactoring
    
    * refactored frontend + fixed upload to upload file and direct to dashboard in one press
    
    * renamed UploadViewManager to UploadFileView + changed Bias Score to Fairness Score
    
    * clean up backend
    
    * refactored backend

---
+++ frontend/src/views/UploadFileView.tsx
@@ -0,0 +1,39 @@
+import React, { useRef } from "react";
+import { UploadFileController } from "../controllers/UploadFileController"
+import { UploadFileInteractor } from "../usecases/UploadFileInteractor";
+import { UploadFilePresenter } from "../presenters/UploadFilePresenter";
+import Button from "./components/Button/Button";
+
+const UploadFileView: React.FC = () => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const presenter = new UploadFilePresenter();
+  const interactor = new UploadFileInteractor();
+  const controller = new UploadFileController(interactor, presenter);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+        controller.handleFileUpload(file);
+    }
+  };
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        accept=".parquet"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        style={{ display: "none" }}
+      />
+      <Button label="Upload Data >" onClick={() => { handleButtonClick(); }} />
+    </div>
+  );
+};
+
+export default UploadFileView;
---
+++ frontend/src/views/pages/MainPage.tsx
@@ -0,0 +1,68 @@
+// src/App.tsx
+
+import React, { useEffect, useState } from "react";
+import "./MainPage.css";
+import Footer from "../components/Footer/Footer";
+import HeroSection from "../components/HeroSection/HeroSection";
+import ProcessStep from "../components/ProcessStep/ProcessStep";
+import Table from "../components/Table/Table"
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const MainPage: React.FC = () => {
+  // const [filename, setFilename] = useState<string>("")
+  // const navigate = useNavigate();
+
+  // const fetchDashboardData = async () => {
+  //   // Simulating a backend call
+  //   const response = await fetch(`http://127.0.0.1:8000/api/metrics`, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({filename: 'value'}),
+  //   });
+
+  //   const dashboardData = await response.json()
+  //   navigate('/dashboard', { dashboardData: { dashboardData } });
+  // };
+
+  // useEffect(
+  //   fetchDashboardData,
+  //   [filename]
+  // )
+
+  // console.log(filename)
+
+  return (
+    <div className="MainPage">
+      {/* Hero Section */}
+      <HeroSection
+        heading="Target fraud detection bias at the source."
+        body="Some sort of subtitle text here. Check out our handy guide ↓ if you need any help!"
+      />
+
+      {/* Process Step / Instruction Section */}
+      <ProcessStep
+        title="Upload your dataset."
+        stepnum={1}
+        body="The format of the dataset must be a parquet file with the following mandatory columns highlighted in the table below."
+      />
+      <Table />
+      <ProcessStep
+        title="View our feedback."
+        stepnum={2}
+        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean molestie mi id sapien posuer."
+      />
+      <ProcessStep
+        title="Adjust and try again!"
+        stepnum={3}
+        body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean molestie mi id sapien posuer."
+      />
+
+      <Footer label="© 2024 Team TripleB" />
+    </div>
+  );
+};
+
+export default MainPage;

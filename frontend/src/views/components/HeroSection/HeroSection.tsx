6bd2a990da96a8a2fde141ec7652dd569e542513
Michelle <michh2005@gmail.com>
2024-11-16 02:17:30 -0500

    modified herosection page to redirect to upload page

--- frontend/src/views/components/HeroSection/HeroSection.tsx
+++ frontend/src/views/components/HeroSection/HeroSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './HeroSection.css';
 import Button from '../Button/Button';
+import { Link } from 'react-router-dom';
 
 type setState = (filename: string) => void;
 
@@ -14,7 +15,9 @@ const HeroSection: React.FC<HeroSectionProps> = ({ heading, body }) => {
         <div className="hero-section">
             <h1 className="hero-heading">{heading}</h1>
             <p className="hero-body">{body}</p>
-            <Button onClick={() => null} label="get started >"/>
+            <Link to="upload" style={{ textDecoration: 'none' }}>  
+                <Button onClick={() => null} label="get started >"/>
+            </Link>
         </div>
     );
 };

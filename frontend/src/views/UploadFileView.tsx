2b875ff3d3e32347217fde32fc3e52164a960c46
Michelle <michh2005@gmail.com>
2024-11-15 21:03:27 -0500

    created changed for upload file button

--- frontend/src/views/UploadFileView.tsx
+++ frontend/src/views/UploadFileView.tsx
@@ -1,23 +1,16 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { UploadFileController } from "../controllers/UploadFileController"
 import { UploadFileInteractor } from "../usecases/UploadFileInteractor";
 import { UploadFilePresenter } from "../presenters/UploadFilePresenter";
-import Button from "./components/Button/Button";
+import { Button } from "@mui/material";
+import UploadIcon from '@mui/icons-material/Upload';
 
-const UploadFileView: React.FC = () => {
+interface Props {
+  handleFileChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const UploadFileView: React.FC<Props> = ({handleFileChange}) => {
   const fileInputRef = useRef<HTMLInputElement | null>(null);
-
-  const presenter = new UploadFilePresenter();
-  const interactor = new UploadFileInteractor();
-  const controller = new UploadFileController(interactor, presenter);
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-        controller.handleFileUpload(file);
-    }
-  };
-
   const handleButtonClick = () => {
     fileInputRef.current?.click();
   };

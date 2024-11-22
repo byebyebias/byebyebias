9931c55be9fc415609223a1cf1474efe6c381928
chloehylee <335732582@gapps.yrdsb.ca>
2024-11-22 03:49:17 -0500

    updated file names

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.js'
+import App from './MainPage.js'
 
 // Trying react router stuff here
 import DashBoardPage from "./pages/Dashboard"

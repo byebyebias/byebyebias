c8cfea08ac72e0be6c07e2c68fb7ad56ea547170
Michelle Huang <50971945+1michhu1@users.noreply.github.com>
2024-11-24 17:26:35 -0500

    Michhhuang/tri 83 allow user to select attributes to measure biasfairness for (#42)
    * got rid of pycache files
    
    * accidentlly commited to another branch, putting changes here
    
    * created changed for upload file button
    
    * fixed hero section button, which now redirects to uploda page
    
    * changed upload button font size
    
    * modified herosection page to redirect to upload page
    
    * upload button now disabled when both upload AND input link are empty
    
    * add logic for ensuring either file OR bucket link are uploaded
    
    * error message for when both file AND link provided
    
    * changed link icon to be in img tag, not as a background url in css
    
    * added pyarrow to requirements
    
    * View results button now has separate handler from Upload button
    
    * fixed typescript errors
    
    * started a test file for upload page
    
    * fixed typescript errors, made upload button same size as choose file button
    
    * added border raidus to upload button
    
    * added footer to upload page, and changed footer background color
    
    * added label for accessiblity and footer
    
    * created a test suite for upload page
    
    * trying esnext for frontend testing
    
    * testing diff configs to make tests pass
    
    * trying esnext in tscondifg node
    
    * trying esnext in tscondifg node
    
    * trying esnext in tscondifg node
    
    * trying esnext in tscondifg node
    
    * trying to add vite/client to tsconfig.json
    
    * removed apiUrl import in mainpage
    
    * trying vite env d ts file in route with more configs
    
    * trying new lines in vite env d ts again
    
    * testing no upload page test file
    
    * testing uploadpage test in another directory
    
    * removed apiurl
    
    * temporarily removed upload page test
    
    * removed upload page test
    
    * added test back
    
    * got rid of random vite api url
    
    * turned all config files back to same as main's

---
+++ frontend/src/views/pages/UploadPage/UploadPage.tsx
@@ -0,0 +1,148 @@
+import { 
+    Typography,
+    Button,
+    Container,
+    Box,
+} from "@mui/material";
+import Grid from '@mui/material/Grid2';
+import { useRef, useState } from "react";
+
+import { UploadFileController } from "../../../controllers/UploadFileController"
+import { UploadFileInteractor } from "../../../usecases/UploadFileInteractor";
+import { UploadFilePresenter } from "../../../presenters/UploadFilePresenter";
+
+import UploadFileView from "../../UploadFileView";
+import styles from "./UploadPage.module.css"
+import Footer from "../../components/Footer/Footer";
+
+const protectedAttributes = ["sender_gender", "sender_race", "sender_age", "receiver_gender", "receiver_race"]
+
+function UploadPage() {
+    const [file, setFile] = useState<File | undefined>(undefined)
+    const [page, setPage] = useState<number>(1)
+    const [link, setLink] = useState<string>("")
+    const linkRef = useRef<HTMLInputElement | null>(null)
+
+    const presenter = new UploadFilePresenter();
+    const interactor = new UploadFileInteractor();
+    const controller = new UploadFileController(interactor, presenter);
+
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newFile = event.target.files?.[0];
+        if (newFile) setFile(newFile)
+    };
+
+    const handleUploadClick = () => {
+        if (file && link) {
+            linkRef.current?.setCustomValidity("Please enter ONLY A FILE or ONLY A LINK")
+            linkRef.current?.reportValidity()
+        } else if (file) {
+            setPage(2)
+        } else if (linkRef.current?.reportValidity()) {
+            setPage(2)
+        } 
+
+    };
+    const handleViewResults = () => {if (file != null) return controller.handleFileUpload(file)};
+
+    const onLinkChange = (e: React.ChangeEvent<HTMLInputElement>) => setLink(e.target.value)
+
+    return (
+        <>
+            <Container
+                maxWidth={false}
+                disableGutters
+                className={styles.container}
+                aria-labelledby="uploadHeader"
+            >
+                {page === 1 &&
+                    <>
+                        <main className={styles.center}>
+                            <Typography variant="h1" fontSize="4em" fontWeight="600" id="uploadHeader" fontFamily="Montserrat">Upload Your Dataset</Typography>
+
+                            <Box sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+                                <UploadFileView handleFileChange={handleFileChange}/>
+                                <Typography variant="body2" fontFamily="Montserrat">{file ? file.name : ''}</Typography>
+                            </Box>
+
+                            <Typography 
+                                variant="h2"
+                                fontSize="2.5em"
+                                fontWeight="500"
+                                fontFamily="Montserrat"
+                            >
+                                or
+                            </Typography>
+
+                            <div style={{position:"relative"}}>
+                                <img alt="" src="linkIcon.png" className={styles.bucketLinkIcon}/>
+                                <input 
+                                    id="bucketInput"
+                                    name="bucketInput"
+                                    className={styles.bucketUrlInput} 
+                                    type="url"
+                                    placeholder="paste public s3 bucket link"
+                                    value={link}
+                                    onChange={onLinkChange}
+                                    ref={linkRef}
+
+                                    // label for accesibility
+                                    aria-label="paste public s3 bucket link"
+                                />
+                            </div>
+
+                            <Button 
+                                onClick={handleUploadClick}
+                                className={styles.uploadButton}
+                                disabled={file == undefined && link == ''}
+                            >
+                                Upload
+                            </Button>
+                        </main>
+
+                    </>
+                }
+
+                {page === 2 && 
+                    <>
+                        <main className={styles.center} aria-live="polite">
+                            <Typography  variant="h1" fontSize="4em" fontWeight="500" id="uploadHeader">Select Attributes</Typography>
+
+                            {/* TODO FOR BUCKET, ADD ACTUAL FILE NAME WHEN CONNECTED WITH S3 INTEGRATION*/}
+                            <Typography variant="h2"  fontSize="1.25em">
+                                Selected attributes in <span className={styles.filename}>{file ? file.name : "INSERTDUMMYBUCKETNAME.parquet"}</span> will be scanned for bias
+                            </Typography>
+                            <Grid 
+                                container 
+                                spacing={{ xs: 2, md: 3 }} 
+                                columns={{xs: 1, sm: 1, md: 2, lg: 3, xl: 3}} 
+                                className={styles.attributeSelection}
+                            >
+                                {protectedAttributes.map((attribute, index) => 
+                                    <Grid size={1} sx={{display: "flex", justifyContent: "center"}}>
+                                        <Button 
+                                            key={index}
+                                            className={styles.attributeButton}
+                                        >
+                                            {attribute}
+                                        </Button>
+                                    </Grid>
+                                )}
+                            </Grid>
+
+                            <Button variant="contained" onClick={handleViewResults}>
+                                View Results
+                            </Button>
+                        </main>
+                    </>
+                }
+            </Container>    
+
+            <footer>
+                <Footer label="made with <3 by team triple b."/>
+            </footer>
+        </>
+    )
+}
+
+export default UploadPage;

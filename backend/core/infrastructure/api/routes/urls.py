a5191df86cff21a444d0c1641c01e02a03eafc34
Michelle <michh2005@gmail.com>
2024-11-26 16:57:44 -0500

    added minor note in urls.py

--- backend/core/infrastructure/api/routes/urls.py
+++ backend/core/infrastructure/api/routes/urls.py
@@ -3,3 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include("backend.core.infrastructure.api.routes.bias_metrics.urls"))
 ]
+
+# add new route to '' by copypasting above path() line but with relevant module in include()

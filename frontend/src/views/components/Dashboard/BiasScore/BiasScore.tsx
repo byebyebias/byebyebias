24e820c616e82abfdda5fdd22508c987853b23f1
Helen <129004013+1zhaohel@users.noreply.github.com>
2024-11-26 17:00:52 -0500

    minor edit to overall score on dashboard

--- frontend/src/views/components/Dashboard/BiasScore/BiasScore.tsx
+++ frontend/src/views/components/Dashboard/BiasScore/BiasScore.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardHeader, Typography } from "@mui/material";
 function BiasScore({score}) {
     return (
         <Card  sx={{flex: "1"}}>
-            <CardHeader title="Dataset Accuracy Score"/>
+            <CardHeader title="Accuracy Score"/>
             <CardContent>
                 <Typography variant="h1">{score}</Typography>
             </CardContent>

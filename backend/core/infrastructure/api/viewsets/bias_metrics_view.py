69c609e2c21f5af28d930154fb0c3348f3f7c004
aerymist <leiliahxy@gmail.com>
2024-12-02 01:04:25 -0500

    Refactored S3 integration to align with CA structure changes

--- backend/core/infrastructure/api/viewsets/bias_metrics_view.py
+++ backend/core/infrastructure/api/viewsets/bias_metrics_view.py
@@ -13,8 +13,13 @@ class BiasMetricsViewSet(ViewSet):
     def controller(self) -> BiasMetricsController:
         return self.viewset_factory.create()
 
-    def get_bias_metrics(self, request: Request, *args, **kwargs) -> Response:
+    def get_bias_metrics_from_file(self, request: Request, *args, **kwargs) -> Response:
         request_files = request.FILES
         request_protected_attributes = json.loads(request.POST.get("protected_attributes"))
-        payload, status = self.controller.get_bias_metrics(request_files, request_protected_attributes)
+        payload, status = self.controller.get_bias_metrics_from_file(request_files, request_protected_attributes)
+        return Response(data=payload, status=status)
+    
+    def get_bias_metrics_from_link(self, request: Request, *args, **kwargs) -> Response:
+        request_protected_attributes = json.loads(request.POST.get("protected_attributes"))
+        payload, status = self.controller.get_bias_metrics_from_link(request, request_protected_attributes)
         return Response(data=payload, status=status)
--- frontend/src/usecases/S3LinkUploadInteractor.ts
+++ frontend/src/usecases/S3LinkUploadInteractor.ts
@@ -3,10 +3,9 @@ import { apiService } from "../services/apiService";
 export class S3LinkUploadInteractor {
 
   async processLink(s3Link: string, selectedAttributes: Array<string>): Promise<any> {
-    const formData = new FormData()
+    const formData = new FormData();
     formData.append("link", s3Link);
     formData.append("protected_attributes", JSON.stringify(selectedAttributes));
-
     const response = await apiService.post("/process_link/", formData);
     if (response.ok) {
       const data = await response.json();
@@ -17,7 +16,7 @@ export class S3LinkUploadInteractor {
         metricResults: data.metric_results,
       };
     } else {
-      throw new Error("File upload failed");
+      throw new Error("S3 link upload failed");
     }
   }
 }

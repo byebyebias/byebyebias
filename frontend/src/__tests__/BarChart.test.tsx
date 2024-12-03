43e482042eb1dae2ddeb9d4311b25386da9289c0
akshata <121204544+aakshataa@users.noreply.github.com>
2024-12-02 22:25:17 -0500

    fixed barchart test (#69)

--- frontend/src/__tests__/BarChart.test.tsx
+++ frontend/src/__tests__/BarChart.test.tsx
@@ -4,8 +4,9 @@ import BarChart from '../views/components/Dashboard/BarChart/BarChart';
 import { ResponsiveBar } from '@nivo/bar';
 
 jest.mock('@nivo/bar', () => ({
-    ResponsiveBar: jest.fn(() => <div>Mocked BarChart</div>),
+    ResponsiveBar: jest.fn(() => <div >Mocked BarChart</div>),
 }));
+
 
 describe('BarChart Component', () => {
     const testData=[
@@ -16,19 +17,89 @@ describe('BarChart Component', () => {
         render(<BarChart data={testData} height={''} width={''}/>);
     })
 
-// checks if mock renders correctly
+    test('renders data and styling correctly', () => {
+      expect(ResponsiveBar).toHaveBeenCalledWith(
+        expect.objectContaining({
+          isFocusable: true,
+          barAriaLabel: expect.any(Function),
+          theme: {
+            labels: {
+              text: {
+                fontSize: "14px",
+                fontFamily: "Montserrat",
+                fontWeight: "400",
+                fill: "333",
+              },
+            },
+            axis: {
+              ticks: {
+                text: {
+                  fontSize: 12,
+                  fontFamily: "Montserrat",
+                  fill: "#333",
+                  maxWidth: "20px",
+                },
+              },
+              legend: {
+                text: {
+                  fontSize: 14,
+                  fontFamily: "Montserrat",
+                  fontWeight: "bold",
+                  fill: "#333",
+                },
+              },
+            },
+          },
+          data: testData,
+          keys: ["score"],
+          indexBy: "protected_attribute",
+          margin: { top: 50, right: 50, bottom: 50, left: 80 },
+          padding: 0.3,
+          valueScale: { type: "linear" }, 
+          indexScale: { type: "band", round: true }, 
+          colors: expect.any(Function),
+          borderColor: {
+            from: "color",
+            modifiers: [["darker", 1.6]],
+          },
+          axisTop: null, 
+          axisRight: null, 
+          axisBottom: {
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "protected_attribute",
+            legendPosition: "middle",
+            legendOffset: 32,
+            truncateTickAt: 0,
+            ariaHidden: true,
+          }, 
+          axisLeft: {
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Score",
+            legendPosition: "middle",
+            legendOffset: -60,
+            ariaHidden: true,
+          }, 
+          labelSkipWidth: 12, 
+          labelSkipHeight: 12, 
+          labelTextColor: expect.any(Function),
+          role: "application",
+        }), 
+        expect.anything() 
+      )
+  });
+
 test("renders barchart", () => {
-    // mocks a ResponsiveBar to use in testing
     
     const barElement = screen.getByText(/Mocked BarChart/i);
     expect(barElement).toBeInTheDocument();
 });
 
-
 test("renders data correctly", () => {
-    // check if the data is passed correctly 
 
-    render(<BarChart data={testData} height={''} width={''}/>);
     expect(ResponsiveBar).toHaveBeenCalledWith(
         expect.objectContaining({
             data: testData,
@@ -37,53 +108,10 @@ test("renders data correctly", () => {
     );
 });
 
-
-test('renders data and styling correctly', () => {
-    
-    expect(ResponsiveBar).toHaveBeenCalledWith(
-      expect.objectContaining({
-        data: testData,
-        keys: ['score'], // Ensure the key is correctly passed
-        indexBy: 'protected_attribute', // Check the 'indexBy' property
-        margin: { top: 50, right: 50, bottom: 50, left: 80 }, // Check margin
-        padding: 0.3, // Check padding
-        valueScale: { type: 'linear' }, // Check valueScale
-        indexScale: { type: 'band', round: true }, // Check indexScale
-        colors: expect.any(Function), // Check colors
-        axisTop: null, // Axis top should be null
-        axisRight: null, // Axis right should be null
-        axisBottom: expect.objectContaining({
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'protected_attribute', // Legend for bottom axis
-          legendPosition: 'middle',
-          legendOffset: 32,
-        }),
-        axisLeft: expect.objectContaining({
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'Score', // Legend for left axis
-          legendPosition: 'middle',
-          legendOffset: -60,
-        }),
-        labelSkipWidth: 12,
-        labelSkipHeight: 12,
-        labelTextColor: {
-          from: 'color',
-          modifiers: [['darker', 1.6]],
-        },
-        role: 'application',
-      }),
-      expect.anything() 
-    );
-  });
-
 test("renders x and y axis correctly", () => {
       expect(ResponsiveBar).toHaveBeenCalled();
 });
 
 })
-    
+
 
